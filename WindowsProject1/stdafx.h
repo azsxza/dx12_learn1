65d5a841ff7a71f034166770165086c25615778e
azsxza <az631132770@gmail.com>
2020-12-05 22:03:42 +0800

    update

--- WindowsProject1/stdafx.h
+++ WindowsProject1/stdafx.h
@@ -10,3 +10,16 @@
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
 #include "d3dx12.h"
+
+HWND hwnd = NULL;
+LPCTSTR WindowName = L"DX12";
+LPCTSTR WindowTitle = L"DX12 Window";
+
+int Width = 800;
+int Height = 600;
+bool FullScreen = false;
+
+bool InitalizeWindow(HINSTANCE hInstace, int ShowWnd, int width, int height, bool fullScreen);
+void mainLoop();
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
